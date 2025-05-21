1be5974154504175eb3da540b58e177ea31fc89c
vairajothi2405 <vairajothivanitha@gmail.com>
2025-05-21 12:29:11 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number"))
+if a>=0 and a>100:
+ print("value is positive and greater than 100")
+elif a>=0 and a<=100:
+ print("value is positive")
+elif a<0 and a>-100:
+ print("value is negative")
+else:
+ print("value is negative and greater than -100")
