ad0e2c08120936a3beec1b934a210e82111b9258
vairajothi2405 <vairajothivanitha@gmail.com>
2025-05-26 10:08:28 +0530

    Add files via upload

--- lo.py
+++ lo.py
@@ -1,9 +1,24 @@
-from flask import Flask,render_template
-login=Flask(__name__)
-@login.route("/")
-def fun():
+from flask import Flask,render_template,request
+import mysql.connector
+logo=Flask(__name__)
+db=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Dhanush@1",
+    database="college"
+)
+cursor=db.cursor()
+@logo.route("/")
+def college():
     return render_template("login.html")
-
+@logo.route('/login',methods=['POST'])
+def login():
+    username=request.form['username']
+    password=request.form['password']
+    insert__query="INSERT INTO tab(name,password)VALUES(%s,%s)"
+    cursor.execute(insert__query,(username,password))
+    db.commit()
+    return "username added to database successfully"
 if __name__=='__main__':
-    login.run()
+    logo.run(debug=True)
     
